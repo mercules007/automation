ee9b5c259012f106800155cc5b1feac32e97e7e3
mercules007 <bughunter1913@gmail.com>
2025-04-08 03:41:08 +0600

    Create result.py

---
+++ result.py
@@ -0,0 +1,455 @@
+✅ Product No: 1
+ 🛑 Name: Fleur Pleated Scrunchie
+ 💰 Price: Tk 1,800
+----------------------------------------
+ ✅ Product No: 2
+ 🛑 Name: Milky Jelly Cleansing Balm
+ 💰 Price: Tk 3,500
+----------------------------------------
+ ✅ Product No: 3
+ 🛑 Name: Cleansing Balm + Bar Duo
+ 💰 Price: Sale price
+Tk 5,300
+Regular price
+Tk 6,000
+----------------------------------------
+ ✅ Product No: 4
+ 🛑 Name: Cleanse + Moisture Duo
+ 💰 Price: Sale price
+Tk 7,000
+Regular price
+Tk 8,000
+----------------------------------------
+ ✅ Product No: 5
+ 🛑 Name: Futuredew Solid
+ 💰 Price: Tk 3,800
+----------------------------------------
+ ✅ Product No: 6
+ 🛑 Name: Futuredew Duo
+ 💰 Price: Sale price
+Tk 7,000
+Regular price
+Tk 8,000
+----------------------------------------
+ ✅ Product No: 7
+ 🛑 Name: Ultralip
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 8
+ 🛑 Name: Lip Line + Ultralip
+ 💰 Price: Sale price
+Tk 5,000
+Regular price
+Tk 5,800
+----------------------------------------
+ ✅ Product No: 9
+ 🛑 Name: Ultralip Duo
+ 💰 Price: Sale price
+Tk 5,600
+Regular price
+Tk 6,300
+----------------------------------------
+ ✅ Product No: 10
+ 🛑 Name: The Beauty Bag
+ 💰 Price: Tk 6,500
+----------------------------------------
+ ✅ Product No: 11
+ 🛑 Name: Mini Beauty Bag
+ 💰 Price: Tk 5,000
+----------------------------------------
+ ✅ Product No: 12
+ 🛑 Name: Balm Dotcom Trio
+ 💰 Price: Sale price
+Tk 6,000
+Regular price
+Tk 6,900
+----------------------------------------
+ ✅ Product No: 13
+ 🛑 Name: Lip Line
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 14
+ 🛑 Name: Cloud Paint Blush
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 15
+ 🛑 Name: Cloud Paint Bronzer
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 16
+ 🛑 Name: Boy Brow
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 17
+ 🛑 Name: Balm Dotcom
+ 💰 Price: Tk 2,300
+----------------------------------------
+ ✅ Product No: 18
+ 🛑 Name: No. 1 Pencil
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 19
+ 🛑 Name: Balm + Line Duo
+ 💰 Price: Sale price
+Tk 4,300
+Regular price
+Tk 4,900
+----------------------------------------
+ ✅ Product No: 20
+ 🛑 Name: Boston Crewneck Sweatshirt
+ 💰 Price: Tk 7,200
+----------------------------------------
+ ✅ Product No: 21
+ 🛑 Name: Shadow Stick
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 22
+ 🛑 Name: Birthday Tee
+ 💰 Price: Sale price
+Tk 3,000
+Regular price
+Tk 5,500
+----------------------------------------
+ ✅ Product No: 23
+ 🛑 Name: Holiday Tee
+ 💰 Price: Sale price
+Tk 2,000
+Regular price
+Tk 5,200
+----------------------------------------
+ ✅ Product No: 24
+ 🛑 Name: Logo Tee
+ 💰 Price: Sale price
+Tk 3,000
+Regular price
+Tk 5,500
+----------------------------------------
+ ✅ Product No: 25
+ 🛑 Name: Wavy Tray
+ 💰 Price: Sale price
+Tk 2,900
+Regular price
+Tk 3,800
+----------------------------------------
+ ✅ Product No: 26
+ 🛑 Name: Glossier You Solid
+ 💰 Price: Tk 5,000
+----------------------------------------
+ ✅ Product No: 27
+ 🛑 Name: Futuredew
+ 💰 Price: Tk 4,300
+----------------------------------------
+ ✅ Product No: 28
+ 🛑 Name: Chrome Beauty Bag
+ 💰 Price: Tk 7,900
+----------------------------------------
+ ✅ Product No: 29
+ 🛑 Name: Boy Brow Arch
+ 💰 Price: Tk 3,500
+----------------------------------------
+ ✅ Product No: 30
+ 🛑 Name: Three Eyed Smiley Mug
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 31
+ 🛑 Name: Classic Cap
+ 💰 Price: Tk 4,300
+----------------------------------------
+ ✅ Product No: 32
+ 🛑 Name: Holiday Hoodie
+ 💰 Price: Sale price
+Tk 8,000
+Regular price
+Tk 12,200
+----------------------------------------
+ ✅ Product No: 33
+ 🛑 Name: Crème de You
+ 💰 Price: Tk 6,500
+----------------------------------------
+ ✅ Product No: 34
+ 🛑 Name: Lash Slick
+ 💰 Price: Tk 2,900
+----------------------------------------
+ ✅ Product No: 35
+ 🛑 Name: Stretch Fluid Foundation
+ 💰 Price: Tk 4,900
+----------------------------------------
+ ✅ Product No: 36
+ 🛑 Name: Stretch Balm Concealer
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 37
+ 🛑 Name: Perfecting Skin Tint
+ 💰 Price: Tk 4,000
+----------------------------------------
+ ✅ Product No: 38
+ 🛑 Name: Milky Jelly Cleansing Bar
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 39
+ 🛑 Name: After Baume
+ 💰 Price: Tk 4,600
+----------------------------------------
+ ✅ Product No: 40
+ 🛑 Name: Milky Jelly Cleanser
+ 💰 Price: Tk 1,800
+----------------------------------------
+ ✅ Product No: 41
+ 🛑 Name: Lip Gloss
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 42
+ 🛑 Name: Generation G
+ 💰 Price: Tk 2,900
+----------------------------------------
+ ✅ Product No: 43
+ 🛑 Name: Body Hero Daily Perfecting Lotion
+ 💰 Price: Tk 4,000
+----------------------------------------
+ ✅ Product No: 44
+ 🛑 Name: Body Hero Daily Oil Wash
+ 💰 Price: Tk 4,000
+----------------------------------------
+ ✅ Product No: 45
+ 🛑 Name: Brow Flick
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 46
+ 🛑 Name: Priming Moisturizer Rich
+ 💰 Price: Tk 5,000
+----------------------------------------
+ ✅ Product No: 47
+ 🛑 Name: Full Orbit
+ 💰 Price: Tk 4,000
+----------------------------------------
+ ✅ Product No: 48
+ 🛑 Name: Birthday Cake Candle
+ 💰 Price: Tk 7,200
+----------------------------------------
+ ✅ Product No: 49
+ 🛑 Name: G Suit
+ 💰 Price: Tk 3,500
+----------------------------------------
+ ✅ Product No: 50
+ 🛑 Name: Hand Cream
+ 💰 Price: Tk 2,900
+----------------------------------------
+ ✅ Product No: 51
+ 🛑 Name: Glossier Candles
+ 💰 Price: Tk 6,500
+----------------------------------------
+ ✅ Product No: 52
+ 🛑 Name: Lidstar
+ 💰 Price: Tk 2,900
+----------------------------------------
+ ✅ Product No: 53
+ 🛑 Name: Universal Pro-Retinol
+ 💰 Price: Tk 5,800
+----------------------------------------
+ ✅ Product No: 54
+ 🛑 Name: Pro Tip
+ 💰 Price: Tk 3,000
+----------------------------------------
+ ✅ Product No: 55
+ 🛑 Name: Monochromes
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 56
+ 🛑 Name: Wowder
+ 💰 Price: Tk 3,500
+----------------------------------------
+ ✅ Product No: 57
+ 🛑 Name: Body Hero Dry-Touch Oil Mist
+ 💰 Price: Tk 4,300
+----------------------------------------
+ ✅ Product No: 58
+ 🛑 Name: Deodorant Refillable Case
+ 💰 Price: Tk 800
+----------------------------------------
+ ✅ Product No: 59
+ 🛑 Name: Deodorant Refill Stick
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 60
+ 🛑 Name: Super Glow
+ 💰 Price: Tk 4,900
+----------------------------------------
+ ✅ Product No: 61
+ 🛑 Name: Super Bounce
+ 💰 Price: Tk 4,900
+----------------------------------------
+ ✅ Product No: 62
+ 🛑 Name: Super Pure
+ 💰 Price: Tk 4,900
+----------------------------------------
+ ✅ Product No: 63
+ 🛑 Name: Haloscope
+ 💰 Price: Tk 3,200
+----------------------------------------
+ ✅ Product No: 64
+ 🛑 Name: Skywash
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 65
+ 🛑 Name: Priming Moisturizer
+ 💰 Price: Tk 3,800
+----------------------------------------
+ ✅ Product No: 66
+ 🛑 Name: Priming Moisturizer Balance
+ 💰 Price: Tk 3,800
+----------------------------------------
+ ✅ Product No: 67
+ 🛑 Name: Milky Oil
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 68
+ 🛑 Name: Cleanser Concentrate
+ 💰 Price: Tk 3,500
+----------------------------------------
+ ✅ Product No: 69
+ 🛑 Name: Solution
+ 💰 Price: Tk 4,300
+----------------------------------------
+ ✅ Product No: 70
+ 🛑 Name: Body Hero Exfoliating Bar
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 71
+ 🛑 Name: Glossier You Solid Refill
+ 💰 Price: Tk 3,600
+----------------------------------------
+ ✅ Product No: 72
+ 🛑 Name: Monochromes Refill
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 73
+ 🛑 Name: Sharpener
+ 💰 Price: Tk 1,300
+----------------------------------------
+ ✅ Product No: 74
+ 🛑 Name: Stretch Face Brush
+ 💰 Price: Tk 4,000
+----------------------------------------
+ ✅ Product No: 75
+ 🛑 Name: Eyeshadow Brush
+ 💰 Price: Tk 2,600
+----------------------------------------
+ ✅ Product No: 76
+ 🛑 Name: Wowder Brush
+ 💰 Price: Tk 2,900
+----------------------------------------
+ ✅ Product No: 77
+ 🛑 Name: Water Bottle
+ 💰 Price: Tk 4,000
+----------------------------------------
+ ✅ Product No: 78
+ 🛑 Name: You Look Good Cap
+ 💰 Price: Tk 4,300
+----------------------------------------
+ ✅ Product No: 79
+ 🛑 Name: Glossier Sweatshirt
+ 💰 Price: Tk 7,900
+----------------------------------------
+ ✅ Product No: 80
+ 🛑 Name: G Cookie Cutter
+ 💰 Price: Tk 800
+----------------------------------------
+ ✅ Product No: 81
+ 🛑 Name: Limited Edition Embroidered Cream Hoodie
+ 💰 Price: Tk 9,300
+----------------------------------------
+ ✅ Product No: 82
+ 🛑 Name: Glossier Utility Bag
+ 💰 Price: Tk 7,900
+----------------------------------------
+ ✅ Product No: 83
+ 🛑 Name: Field Greens Blanket
+ 💰 Price: Tk 9,300
+----------------------------------------
+ ✅ Product No: 84
+ 🛑 Name: Socks
+ 💰 Price: Tk 800
+----------------------------------------
+ ✅ Product No: 85
+ 🛑 Name: Duffle Bag
+ 💰 Price: Tk 7,900
+----------------------------------------
+ ✅ Product No: 86
+ 🛑 Name: Terrazzo Hoodie
+ 💰 Price: Tk 9,300
+----------------------------------------
+ ✅ Product No: 87
+ 🛑 Name: Original Pink Hoodie
+ 💰 Price: Tk 8,600
+----------------------------------------
+ ✅ Product No: 88
+ 🛑 Name: Lash Slick Duo
+ 💰 Price: Sale price
+Tk 5,200
+Regular price
+Tk 5,800
+----------------------------------------
+ ✅ Product No: 89
+ 🛑 Name: Body Hero Duo
+ 💰 Price: Sale price
+Tk 7,200
+Regular price
+Tk 8,000
+----------------------------------------
+ ✅ Product No: 90
+ 🛑 Name: The Complete Body Hero Collection
+ 💰 Price: Sale price
+Tk 13,200
+Regular price
+Tk 14,900
+----------------------------------------
+ ✅ Product No: 91
+ 🛑 Name: Glossier Brow Duo
+ 💰 Price: Sale price
+Tk 5,900
+Regular price
+Tk 6,600
+----------------------------------------
+ ✅ Product No: 92
+ 🛑 Name: Cloud Paint Duo
+ 💰 Price: Sale price
+Tk 5,500
+Regular price
+Tk 6,300
+----------------------------------------
+ ✅ Product No: 93
+ 🛑 Name: Sip + Smile Duo
+ 💰 Price: Sale price
+Tk 4,300
+Regular price
+Tk 4,900
+----------------------------------------
+ ✅ Product No: 94
+ 🛑 Name: Ultra Cap Duo
+ 💰 Price: Sale price
+Tk 6,500
+Regular price
+Tk 7,500
+----------------------------------------
+ ✅ Product No: 95
+ 🛑 Name: Day Off Set
+ 💰 Price: Sale price
+Tk 10,000
+Regular price
+Tk 11,700
+----------------------------------------
+ ✅ Product No: 96
+ 🛑 Name: Deodorant
+ 💰 Price: Tk 3,300
+----------------------------------------
+ ✅ Product No: 97
+ 🛑 Name: Cherry Lock
+ 💰 Price: Tk 4,000
+----------------------------------------
+ ✅ Product No: 98
+ 🛑 Name: The Super Pack
+ 💰 Price: Sale price
+Tk 12,600
+Regular price
+Tk 14,600
+----------------------------------------
